5cfc79a3343cdf57c92f46ab73901eaf1842f9db
piyush <priyanshudevilliers@gmail.com>
2024-12-31 02:41:14 +0530

    without lamda demo

---
+++ 02_WithoutLambda/src/Main.java
@@ -0,0 +1,7 @@
+//We don't need the implementation class when we use the lambda function
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new SoftwareEngg();
+        System.out.println(employee.getName());
+    }
+}
---
+++ 02_WithoutLambda/src/SoftwareEngg.java
@@ -0,0 +1,7 @@
+//We don't need the implementation class when we use the lambda function
+public class SoftwareEngg implements Employee{
+    @Override
+    public String getName() {
+        return "Software engineer";
+    }
+}
