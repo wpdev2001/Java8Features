af1e31aba343b4b0fa56ad1b8142dc7736d9689d
test <priyanshudevilliers@gmail.com>
2024-12-31 12:43:19 +0000

    list sorting using comparator and lambda

---
+++ 07_ListSortComparatorDemoWithLambda/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+//Descending sort using Comparator with using lambda function
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+
+        list.add(2);
+        list.add(32);
+        list.add(22);
+        list.add(322);
+        list.add(20);
+        list.add(25);
+
+//        Collections.sort(list,(a,b) -> a-b); --> Ascending sort
+        Collections.sort(list,(a,b) -> b-a); // --> For descending sorting
+        System.out.println(list);
+
+    }
+}
