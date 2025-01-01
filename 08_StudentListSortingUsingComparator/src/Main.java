7570bc3292e4b01867609da2cd294ddf2e80343f
piyush <priyanshudevilliers@gmail.com>
2025-01-01 17:16:49 +0530

    Student list sorting using comparator

---
+++ 08_StudentListSortingUsingComparator/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    
+    public static void main(String[] args) {
+
+        List<Student> slist = new ArrayList<>();
+
+        slist.add(new Student(101,"AAA",89.0));
+        slist.add(new Student(104,"DDD",79.0));
+        slist.add(new Student(103,"CCC",99.0));
+        slist.add(new Student(102,"BBB",69.0));
+
+        System.out.println("Student list before sorting : " + slist);
+
+        Collections.sort(slist,(a,b) -> b.getRno() - a.getRno());
+
+        System.out.println("Student list before sorting : " + slist);
+
+    }
+}
---
+++ 08_StudentListSortingUsingComparator/src/Student.java
@@ -0,0 +1,44 @@
+public class Student {
+    private int rno;
+    private String name;
+    private Double per;
+
+    public int getRno() {
+        return rno;
+    }
+
+    public void setRno(int rno) {
+        this.rno = rno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPer() {
+        return per;
+    }
+
+    public void setPer(Double per) {
+        this.per = per;
+    }
+
+    public Student(int rno, String name, Double per) {
+        this.rno = rno;
+        this.name = name;
+        this.per = per;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "rno=" + rno +
+                ", name='" + name + '\'' +
+                ", per=" + per +
+                '}';
+    }
+}
